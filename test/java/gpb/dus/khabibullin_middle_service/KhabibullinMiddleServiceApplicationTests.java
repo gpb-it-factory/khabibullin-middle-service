988c23a55c64e854cac84faa1d259dc870196636
masfuj <gooddayemail@gmail.com>
2024-06-14 17:20:59 +0300

    Задание 5. Создан репозиторий для middle-сервиса

---
+++ test/java/gpb/dus/khabibullin_middle_service/KhabibullinMiddleServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package gpb.dus.khabibullin_middle_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KhabibullinMiddleServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
