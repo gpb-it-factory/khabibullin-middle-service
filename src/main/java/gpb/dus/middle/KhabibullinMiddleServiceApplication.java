bc18905fc90d24d66258c829aaa0ad7e5d14a368
Ildus <112579759+DusynHub@users.noreply.github.com>
2024-06-28 20:35:51 +0300

    Задание 5. Создан репозиторий для middle-сервиса (#2)
    * Задание 5. Создан репозиторий для middle-сервиса
    
    * Задание 5. Внесены правки согласно ревью и в структуру папок
    
    * Задание 5. Возвращён степ build без тестов

---
+++ src/main/java/gpb/dus/middle/KhabibullinMiddleServiceApplication.java
@@ -0,0 +1,13 @@
+package gpb.dus.middle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KhabibullinMiddleServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KhabibullinMiddleServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gpb/dus/middle/KhabibullinMiddleServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package gpb.dus.middle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KhabibullinMiddleServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
